81b84af781a64e0d6b189f1e3ff70aaa577e6ece
Frederik Nielsen <fniel22@student.aau.dk>
2023-04-18 12:58:29 +0200

    Made a first try home screen

--- app/src/main/java/com/example/pma_ek/MainActivity.java
+++ app/src/main/java/com/example/pma_ek/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.pma_ek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
