9d771fe56206a234f909d5314d1713459d32f019
Frederik Nielsen <fniel22@student.aau.dk>
2023-04-19 00:45:36 +0200

    Made the button go to run activity_main.xml

--- app/src/main/java/com/example/pma_ek/MainActivity.java
+++ app/src/main/java/com/example/pma_ek/MainActivity.java
@@ -2,19 +2,31 @@ package com.example.pma_ek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityRun();
+            }
+        });
     }
 
-
-
+    private void openActivityRun() {
+        Intent intent = new Intent(this, Run.class);
+        startActivity(intent);
+    }
 }
