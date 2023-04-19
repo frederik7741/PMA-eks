a8ddf59dce49e7661421015814447066f70b86c3
Bisgaardo <mbisga22@student.aau.dk>
2023-04-19 09:23:16 +0200

    Added a gps feature

--- app/src/main/java/com/example/pma_ek/Run.java
+++ app/src/main/java/com/example/pma_ek/Run.java
@@ -1,16 +1,24 @@
 package com.example.pma_ek;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.Manifest;
 
-public class Run extends AppCompatActivity implements SensorEventListener {
+
+public class Run extends AppCompatActivity implements SensorEventListener, LocationListener {
 
     private SensorManager sensorManager;
     private TextView RunningSpeed;
@@ -20,7 +28,15 @@ public class Run extends AppCompatActivity implements SensorEventListener {
 
     private TextView elapsedTimeTextView;
 
+    private LocationManager locationManager;
+    private Location previousLocation;
+
+    private TextView distanceTextView;
     private long startTimeMillis = 0;
+    private float totalDistance = 0;
+
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +50,26 @@ public class Run extends AppCompatActivity implements SensorEventListener {
         // initialize the RunningSpeed TextView
         RunningSpeed = findViewById(R.id.running_speed_textview);
         elapsedTimeTextView = findViewById(R.id.elapsed_time_textview);
-    }
 
+        // initialize the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // initialize the distance TextView
+        distanceTextView = findViewById(R.id.distance_textview);
+    }
+    private boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+            return false;
+        }
+        // Permission is already granted
+        return true;
+    }
 
     @Override
     protected void onResume() {
@@ -43,15 +77,51 @@ public class Run extends AppCompatActivity implements SensorEventListener {
         // register the sensor listener
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
-        startTimeMillis = System.currentTimeMillis();
+        // check if location permission is granted
+        if (checkLocationPermission()) {
+            // request location updates
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
 
+        startTimeMillis = System.currentTimeMillis();
     }
+
 
     @Override
     protected void onPause() {
         super.onPause();
         // unregister the sensor listener
         sensorManager.unregisterListener(this);
+
+        // check if location permission is granted
+        if (checkLocationPermission()) {
+            // remove location updates
+            locationManager.removeUpdates(this);
+        }
+
+        // calculate the elapsed time
+        long elapsedTimeMillis = System.currentTimeMillis() - startTimeMillis;
+        long elapsedTimeSeconds = elapsedTimeMillis / 1000;
+        String formattedElapsedTime = String.format("%d seconds", elapsedTimeSeconds);
+        elapsedTimeTextView.setText("Elapsed time: " + formattedElapsedTime);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            // calculate the distance traveled
+            if (previousLocation == null) {
+                previousLocation = location;
+            }
+            float distance = location.distanceTo(previousLocation);
+            totalDistance += distance;
+            previousLocation = location;
+
+            // update the distance TextView
+            String formattedDistance = String.format("%.2f", totalDistance);
+            distanceTextView.setText("Distance: " + formattedDistance + " m");
+        }
     }
 
     @Override
