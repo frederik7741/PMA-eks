00a6438ab3b246409094604668fa2051ce834026
CakeDuty <jonasboy10@hotmail.dk>
2023-04-20 13:55:21 +0200

    Added difficulty buttons

--- app/src/main/java/com/example/pma_ek/MainActivity.java
+++ app/src/main/java/com/example/pma_ek/MainActivity.java
@@ -4,25 +4,66 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button button;
+    private Button easyButton, mediumButton, hardButton;
+    private int Difficulty = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Changes
-        button = (Button) findViewById(R.id.button);
+
+        // Initialize the buttons and set click listeners
+        easyButton = findViewById(R.id.easy_button);
+        mediumButton = findViewById(R.id.medium_button);
+        hardButton = findViewById(R.id.hard_button);
+
+        easyButton.setOnClickListener(this);
+        mediumButton.setOnClickListener(this);
+        hardButton.setOnClickListener(this);
+
+        button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivityRun();
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Determine which button was clicked
+        int buttonId = v.getId();
+        if (buttonId == R.id.easy_button) {
+            setActiveDifficulty(1);
+        } else if (buttonId == R.id.medium_button) {
+            setActiveDifficulty(2);
+        } else if (buttonId == R.id.hard_button) {
+            setActiveDifficulty(3);
+        }
+    }
+
+    private void setActiveDifficulty(int difficulty) {
+        Difficulty = difficulty;
+        // Deactivate the previously active button
+        if (Difficulty == 1) {
+            easyButton.setActivated(true);
+            mediumButton.setActivated(false);
+            hardButton.setActivated(false);
+        } else if (Difficulty == 2) {
+            easyButton.setActivated(false);
+            mediumButton.setActivated(true);
+            hardButton.setActivated(false);
+        } else if (Difficulty == 3) {
+            easyButton.setActivated(false);
+            mediumButton.setActivated(false);
+            hardButton.setActivated(true);
+        }
     }
 
     private void openActivityRun() {
